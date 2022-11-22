549dbd62c9b3da266bea78e16bd2c568ada4a8b5
ferdyhaspin <ferdihaspin@gmail.com>
2022-11-22 16:13:39 +0700

    prepare data

---
+++ app/src/main/java/org/ferdyhaspin/jetcoffee/model/Menu.kt
@@ -0,0 +1,22 @@
+package org.ferdyhaspin.jetcoffee.model
+
+import org.ferdyhaspin.jetcoffee.R
+
+/**
+ * Created by ferdyhaspin on 22/11/22.
+ */
+
+data class Menu(
+    val image: Int,
+    val title: String,
+    val price: String,
+)
+
+val dummyMenu = listOf(
+    Menu(R.drawable.menu1, "Tiramisu Coffee Milk", "Rp 28.000"),
+    Menu(R.drawable.menu2, "Pumpkin Spice Latte", "Rp 18.000"),
+    Menu(R.drawable.menu3, "Light Cappuccino", "Rp 20.000"),
+    Menu(R.drawable.menu4, "Choco Creamy Latte", "Rp 16.000"),
+)
+
+val dummyBestSellerMenu = dummyMenu.shuffled()
